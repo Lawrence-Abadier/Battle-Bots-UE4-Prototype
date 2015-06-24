8fac815a6e24cd5c1702c56090d844bb90f23e1f
lawrence-abadier <lawrenceabadier@hotmail.com>
2015-06-24 12:49:28 -0700

    Add default spectator pawn

---
+++ BattleBots/Online/BBotsSpectatorPawn.cpp
@@ -0,0 +1,14 @@
+// Copyright 2015 VMR Games, Inc. All Rights Reserved.
+
+#include "BattleBots.h"
+#include "BBotsSpectatorPawn.h"
+
+
+
+
+
+ABBotsSpectatorPawn::ABBotsSpectatorPawn(const class FObjectInitializer& ObjectInitializer)
+  :Super(ObjectInitializer)
+{
+  bAddDefaultMovementBindings = true;
+}
---
+++ BattleBots/Online/BBotsSpectatorPawn.h
@@ -0,0 +1,19 @@
+// Copyright 2015 VMR Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "GameFramework/SpectatorPawn.h"
+#include "BBotsSpectatorPawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEBOTS_API ABBotsSpectatorPawn : public ASpectatorPawn
+{
+	GENERATED_BODY()
+	
+public:
+  ABBotsSpectatorPawn(const class FObjectInitializer& ObjectInitializer);
+	
+};
