0c3dcdbbcc852daeb314f485244fff13bcbd7720
lawrence-abadier <lawrenceabadier@hotmail.com>
2015-06-26 23:50:51 -0700

    Add static HUD class

---
+++ BattleBots/UI/BBotsHUD.h
@@ -0,0 +1,19 @@
+// Copyright 2015 VMR Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "GameFramework/HUD.h"
+#include "BBotsHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEBOTS_API ABBotsHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
